8be69c681d819d94d9c892385ca1ddfffb5c4955
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-25 17:07:38 +0200

    cleaned script and comments

--- src/script.js
+++ src/script.js
@@ -1,116 +1,95 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-
-/**
- * Base
- */
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 // Textures
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load('/textures/matcaps/matcap.png')
+const matcapTexture = textureLoader.load('/textures/matcaps/matcap.png');
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Materials
+// Material
+const material = new THREE.MeshMatcapMaterial();
+material.side = THREE.DoubleSide;
+material.matcap = matcapTexture;
 
-const material = new THREE.MeshMatcapMaterial()
-material.side = THREE.DoubleSide
-material.matcap = matcapTexture
+// gtlf Loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
 
-// Geometries
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('/draco/')
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 
-const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
-
+// Geometry
 let model;
 
 gltfLoader.load(
   '/models/deprojet.glb',
-  (gltf) =>
-  {
-    model = gltf.scene
+  (gltf) => {
+    model = gltf.scene;
     model.traverse((o) => {
       if (o.isMesh) o.material = material;
     });
-    model.scale.set(1, 1, 1)
-    scene.add(model)
+    model.scale.set(1, 1, 1);
+    scene.add(model);
   }
 )
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
+};
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0,0,20)
-scene.add(camera)
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(0,0,20);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
+// Animate
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+function tick() {
+  const elapsedTime = clock.getElapsedTime();
+  // Rotate model
+  if (model) model.rotation.y = 0.1 * elapsedTime;
+  if (model) model.rotation.x = 0.1 * elapsedTime;
+  // Update controls
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-    if (model) model.rotation.y = 0.1 * elapsedTime;
-    if (model) model.rotation.x = 0.1 * elapsedTime;
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
